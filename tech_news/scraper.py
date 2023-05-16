fb3a0d76a139041b3a57442a9581af595cfb38a6
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-16 18:31:49 -0300

    feat: REQ 1 - add func fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+import time
+
+
 # Requisito 1 - Crie a função fetch
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
