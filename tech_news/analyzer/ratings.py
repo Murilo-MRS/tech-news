adc1321fd613235ac6e76b4ee06277e2aca771cd
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-17 11:48:25 -0300

    feat: REQ 10 - add to_5

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,13 @@
+from collections import Counter
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    category_count = Counter(
+        [new["category"] for new in search_news({})]
+    ).most_common(5)
+
+    category_count.sort(key=lambda x: (-x[1], x[0]))
+
+    return [category[0] for category in category_count]
