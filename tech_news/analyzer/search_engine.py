55a779474649dcf33abc1640b4e499d0fb8ccd27
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-17 10:29:09 -0300

    feat: REQ 7 - add search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+# from datetime import datetime
+from tech_news.database import search_news
+
+
+# def format_list(news):
+#     return [(new["title"], new["url"]) for new in news]
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """case insentive https://sparkbyexamples.com/
+    mongodb/mongodb-query-case-insensitive/"""
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+    formated_list = [(new["title"], new["url"]) for new in news_list]
+    return formated_list
 
 
 # Requisito 8
